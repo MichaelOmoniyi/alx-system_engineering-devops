e30618e22920c024a15c153defb63e7626d570f9
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-11-30 12:39:36 +0000

    Update file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,6 @@
-# Installs and configures nginx server
+# Installs a Nginx server
 
-package { 'nginx':
-    ensure => 'present',
-}
-
-exec { 'install':
-    command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-    provider => shell,
-}
-
-exec { 'Hello':
-    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-    provider => shell,
-}
-
-exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/https://omons-portfolio-page.netlify.app\/;\\n\\t}/" /etc/nginx/sites-available/default';
-    provider => shell,
-}
-
-exec { 'run':
-    command  => 'sudo service nginx restart',
-    provider => shell,
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/MichaelOmoniyi permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
