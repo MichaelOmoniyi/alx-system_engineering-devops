990a32d89d4551803b11fa61bdb182726fb4b34b
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-12-08 12:26:49 +0000

    Update puppet configuration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,5 +2,5 @@
 
 exec {'header':
     provider => shell,
-    command  => 'sed -i (s/server_name _;\n\tserver_name _;\n\tadd_header X-Served-By \$hostname;',
+    command  => 'sed -i "s/server_name _;\n\tserver_name _;\n\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-enabled/default',
             }
