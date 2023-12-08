25471b15f53526c1c6b9a92417ab026059d704d3
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2023-12-08 12:20:33 +0000

    Upload puppet automation for creation of custom HTTP header response

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,6 @@
+# automate the task of creating a custom HTTP header response
+
+exec {'header':
+    provider => shell,
+    command  => 'sed -i (s/server_name _;\n\tserver_name _;\n\tadd_header X-Served-By \$hostname;',
+            }
