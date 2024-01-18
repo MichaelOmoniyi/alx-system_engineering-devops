5a65cba9109c5105cb70b28bcf8cb3951d0f209a
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-18 14:45:02 +0000

    Upload solution to task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Returns the number of subscribers (not active users, total subscribers) for a given subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit
+
+    Args:
+        subreddit: the subreddit to be checked
+
+    Return:
+        0, if subreddit is invalid else the number or subscribers
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = "Google Chrome Version 81.0.4044.129"
+    header = {"User-Agent": user_agent}
+    response = requests.get(url, headers=header)
+
+    try:
+        data = response.json()
+        subscribers = data.get('data').get('subscribers')
+        return(subscribers)
+    except Exception:
+        return 0
