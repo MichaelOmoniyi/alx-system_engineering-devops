74bfafb9725881ab756289e8c02f38648d30e859
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-18 21:41:22 +0000

    Upload solution to task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Quereies the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """ Prints the titles of the first 10 hot posts for a given subreddit """
+
+    if subreddit is None and not isinstance(subreddit, str):
+        return None
+
+    params = {"where": "popular", "limit": 10}
+    header = {"User-Agent": "Google Chrome Version 81.0.4044.129"}
+    base_url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    response = get(base_url, params=params, headers=header)
+
+    try:
+        data = response.json()
+        data_list = data.get('data').get('children')
+        
+        for data in data_list:
+            print(data.get('data').get('title'))
+    except Exception:
+        print("Not found")
+        return 0
