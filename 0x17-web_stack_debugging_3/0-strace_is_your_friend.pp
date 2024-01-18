2faffe38cbec7a06a6053623124e92ed75675b93
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-18 00:54:02 +0000

    Upload solution to task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# Replaces a line in a file in server
+
+$file = '/var/www/html/wp-setting.php'
+
+exec { 'replace_line':
+    command => "sed -i 's/phpp/php/g' ${file}",
+    path    => ['/bin', '/usr/bin']
+}
