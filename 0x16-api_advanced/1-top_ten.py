5abcef2543219d7ba5e28001d47f85f4cbb47ff8
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-19 01:24:08 +0000

    Update solution to task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,26 +7,21 @@ from requests import get
 
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
+    """ Prints the titles of the first 10 hot posts for a given subreddit """
 
-    if subreddit is None or not isinstance(subreddit, str):
+    if subreddit is None and not isinstance(subreddit, str):
         print("None")
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
+    params = {"limit": 10}
+    header = {"User-Agent": "Google Chrome Version 81.0.4044.129"}
+    base_url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    response = get(base_url, params=params, headers=header)
 
     try:
-        my_data = results.get('data').get('children')
+        data = response.json()
+        data_list = data.get('data').get('children')
 
-        for i in my_data:
-            print(i.get('data').get('title'))
-
+        for data in data_list:
+            print(data.get('data').get('title'))
     except Exception:
         print("None")
