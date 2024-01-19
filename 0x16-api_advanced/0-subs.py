20e966ca8364b03a54912809546e8c21e3c41d50
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2024-01-19 00:58:04 +0000

    Update file to conform with pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Returns the number of subscribers (not active users, total subscribers) for a given subreddit
+Returns the number of subscribers (not active users, total subscribers)
+for a given subreddit
 """
 import requests
 
@@ -18,7 +19,7 @@ def number_of_subscribers(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         return None
-    
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     user_agent = "Google Chrome Version 81.0.4044.129"
     header = {"User-Agent": user_agent}
@@ -27,6 +28,6 @@ def number_of_subscribers(subreddit):
     try:
         data = response.json()
         subscribers = data.get('data').get('subscribers')
-        return(subscribers)
+        return (subscribers)
     except Exception:
         return 0
